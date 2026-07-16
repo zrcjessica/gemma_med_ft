cf5bf11857a78b1dd6114ddf502c9d6fd140635b
Jessica Zhou <jessicazhou96@gmail.com>
2026-07-16 13:04:10 -0400

    Make eval env build idempotent too

--- scripts/build_eval_env.sh
+++ scripts/build_eval_env.sh
@@ -8,7 +8,7 @@ export HF_HOME=/gpfs/data/oermannlab/users/zhouj14/hf
 export UV_CACHE_DIR=/gpfs/data/oermannlab/users/zhouj14/.uv_cache
 
 cd "${REPO_ROOT}"
-uv venv --python 3.11 .venv-eval
+uv venv --clear --python 3.11 .venv-eval
 source .venv-eval/bin/activate
 uv pip install "vllm==0.9.1" "transformers>=4.53" "datasets>=3.6" pandas rich
 
